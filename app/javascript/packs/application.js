bb8ba7e2171bcea77378acb6baf8bd097d82a4b8
codingmasawe <44496652+codingmasawe@users.noreply.github.com>
2021-05-01 22:23:19 +0200

    added button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,5 @@ ActiveStorage.start()
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 require("stylesheets/application.scss")
+
+import "@fontawesome/fontawesome-free/css/all"
