0367c78d2783dae442e54d4181c71b3cf226acfb
codingmasawe <44496652+codingmasawe@users.noreply.github.com>
2021-05-05 20:58:43 +0200

    updated seeds and devise index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: 'wuennik@live.com', password: 'verySecret', password_confirmation: 'verySecret')
 
 30.times do
   Course.create([{
   title: Faker::Educator.course_name,
-  description: Faker::ChuckNorris.fact
+  description: Faker::ChuckNorris.fact,
+  user_id: User.first.id
   }])
 end
